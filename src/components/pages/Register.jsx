874ef7c4d7038cd081da6dece7bb78dea9ac1e55
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2024-05-26 11:04:19 +0600

    auth provider added

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -25,6 +25,12 @@ export const Register = () => {
                             </label>
                             <input type="password" placeholder="password" className="input input-bordered" required />
                         </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input type="password" placeholder=" confirm password" className="input input-bordered" required />
+                        </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Register</button>
                         </div>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsIRcaGeulqgaql8fioDfPQvkZ96gUmz4",
-  authDomain: "computer-world-practicetask.firebaseapp.com",
-  projectId: "computer-world-practicetask",
-  storageBucket: "computer-world-practicetask.appspot.com",
-  messagingSenderId: "326383754876",
-  appId: "1:326383754876:web:96a28e98ad81b9ba81b3f9",
-  measurementId: "G-3XNFMZSPST"
+  apiKey: import.meta.env.VITE_apiKey, 
+  authDomain: import.meta.env.VITE_authDomain, 
+  projectId: import.meta.env.VITE_projectId, 
+  storageBucket: import.meta.env.VITE_storageBucket, 
+  messagingSenderId: import.meta.env.VITE_messagingSenderId, 
+  appId: import.meta.env.VITE_appId, 
+  measurementId: import.meta.env.VITE_measurementId, 
 };
 
 // Initialize Firebase
